3a676386b2186363b9e12f8b72a1807900bbea40
DannySe <d.seledtsova@gmail.com>
2018-06-09 21:08:01 +0200

    model post created

--- src/app/model/post.ts
+++ src/app/model/post.ts
@@ -1,2 +1,13 @@
+import { Comment } from "./comment";
+
 export class Post {
+    title : string;
+    text : string;
+    location : string;
+    grapes : string[];
+    hashtags : string[];
+    upvotes : number;
+    date : string; 
+    author : string; 
+    comments : Comment[];
 }
