ebf4076a04af388928e26c106b7feec0ca9e2616
DannySe <d.seledtsova@gmail.com>
2018-06-09 22:23:20 +0200

    added the models

--- src/app/main-frame/main-frame.component.ts
+++ src/app/main-frame/main-frame.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from "./../model/post";
 
 @Component({
@@ -7,7 +7,17 @@ import { Post } from "./../model/post";
   styleUrls: ['./main-frame.component.css']
 })
 
-export class MainFrameComponent {
+export class MainFrameComponent implements OnInit {
+
 posts : Post[];
+
+ngOnInit(): void {
+  let post : Post; 
+  post.title = "Test Title";
+  post.text = "lorem ipsum dolor amet";
+  this.posts[0] = post;
+  this.posts[1] = post;
+  this.posts[2] = post;
+}
 
 }
