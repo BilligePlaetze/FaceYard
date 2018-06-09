5f4995a6d84b4352d9ef32908271679852056cc0
soeren <elias.soeren@gmail.com>
2018-06-09 20:34:42 +0200

    created structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { QueryComponent } from './query/query.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { SettingsComponent } from './settings/settings.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { ServiceComponent } from './service/service.component';
+import { MainFrameComponent } from './main-frame/main-frame.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    CommentComponent,
+    CreatePostComponent,
+    QueryComponent,
+    NewsFeedComponent,
+    SettingsComponent,
+    TutorialComponent,
+    ServiceComponent,
+    MainFrameComponent
   ],
   imports: [
     BrowserModule
