bcfbeaab3541c12dc96e8b37a198cbeb35b7c3bf
soeren <elias.soeren@gmail.com>
2018-06-10 00:51:01 +0200

    added dropdowns and added default values

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Post } from '../model/post'
+import { SettingsComponent } from '../settings/settings.component';
 @Component({
   selector: 'create-post',
   templateUrl: './create-post.component.html',
@@ -9,7 +10,17 @@ export class CreatePostComponent implements OnInit {
 
   constructor() { }
 
+  settings : Post;
+
+
   ngOnInit() {
+this.settings = new Post();
+    this.settings.author = "Helmut Scharnweber";
+    this.settings.grapes = ["Riesling", "Pinot Noir"];
+    this.settings.hashtags = ["Schädlingsbefall","Technik"];
+    this.settings.location = "regional";
   }
+
+  
 
 }
