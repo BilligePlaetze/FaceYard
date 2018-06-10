cea0b29eaffc43a8a11f24e1e4a11598bb464780
DannySe <d.seledtsova@gmail.com>
2018-06-10 02:01:47 +0200

    sharing is caring

--- src/app/main-frame/main-frame.component.ts
+++ src/app/main-frame/main-frame.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 import { Post } from "./../model/post";
+import { MaterializeAction } from 'angular2-materialize';
+
 
 @Component({
   selector: 'main-frame',
   templateUrl: './main-frame.component.html',
-  styleUrls: ['./main-frame.component.css']
+  styleUrls: ['./main-frame.component.css'],
 })
 
 export class MainFrameComponent implements OnInit {
 
   posts : Array<Post> = [];
+  parentTitle : Post;
+  modalActions1 = new EventEmitter<string|MaterializeAction>();
+  
+  model1Params = [
+    {
+      dismissible: false,
+      complete: function() { console.log('Closed'); }
+    }
+  ]
+  openModal1(p : Post) {
+    this.parentTitle = p;
+    this.modalActions1.emit({action:"modal",params:['open']});
+  }
+  closeModal1() {
+    this.modalActions1.emit({action:"modal",params:['close']});
+  }
 
   ngOnInit(): void {
     let post : Post = new Post();
+    let post2 : Post = new Post();
     post.title = "Test Title";
-    post.text = "lorem ipsum dolor amet";
+    post.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    post2.title = "Testing testing";
+    post2.text = "Roses are red, wine is also red. Poems are hard. Wine.";
     this.posts[0] = post;
-    this.posts[1] = post;
+    this.posts[1] = post2;
 }
 
 }
